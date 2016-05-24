b7aa14ebd0ec4473760d85bf8ca6f7a3694ae798
David <davidazevedo1996@hotmail.com>
2016-05-24 11:56:06 +0100

    Minor Fix

--- TrabalhoFinalLPOO/src/projeto/logic/PowerUp.java
+++ TrabalhoFinalLPOO/src/projeto/logic/PowerUp.java
@@ -4,9 +4,10 @@ public class PowerUp extends GameObject {
 
 	public PowerUp() {
 		super(null,null,null);
-		m_Collider = new CircleCollider(10,0,new Vector2(100,100),"PowerUp", false, 1);   
+		m_Collider = new CircleCollider(10,0,new Vector2(100,100),"PowerUp", false, 1);
 		m_Obj = new Obj(new Rectangulo(100,100, 10), "circulo.png", new Rectangulo(0,0,1,1));
 		m_Collider.addListener(this);
+		m_Collider.trigger = true;
 	}
 
 	@Override
